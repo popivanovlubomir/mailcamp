80b3680b3b4658f54e8197a14dcd201fc12a0fab
Lubomir Popivanov <lpopivanov@sbnd.net>
2017-02-21 17:50:10 +0200

    setup the tinymce editor parameters

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -6,5 +6,15 @@
 <script src="//cloud.tinymce.com/stable/tinymce.min.js?apiKey=fivlafxxjgtd70jdo5zit6ogd7yt77on1nsikvctrzwhlsz2"></script>
 
 <script type="text/javascript">
-    tinymce.init({ selector:'.richtext' });
+    tinymce.init({
+        selector:'.richtext',
+        menubar: false,
+        plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table contextmenu paste code'
+        ],
+        toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        content_css: '//www.tinymce.com/css/codepen.min.css'
+    });
 </script>
